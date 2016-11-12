bfa44bf44c6a1cb0eb37993d6454c9fcbec04193
Dmitriy Ovchinnikov <ovchinnikov.d94@gmail.com>
2016-11-12 11:47:55 +0300

    Initial commit #2

---
+++ vingrad-strassen.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def simple_miltiply(a, b):
+    assert(a.shape[0] != b.shape[0] and a.shape[1] != b.shape[1])
+    result = np.zeros(a.shape)
+    for i in range(a.shape[0]):
+        for j in range()
